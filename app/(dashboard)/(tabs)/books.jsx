52c9e08d74fd45963f223e4c5144d0b59b76c69a
akash-uppula <akashuppula02@gmail.com>
2026-09-04 15:33:39 +0530

    27 done

--- app/(dashboard)/(tabs)/books.jsx
+++ app/(dashboard)/(tabs)/books.jsx
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
 
   header: {
     alignItems: "center",
-    marginBottom: 20,
+    marginBottom: 10,
   },
 
   title: {
--- app/(dashboard)/(tabs)/create.jsx
+++ app/(dashboard)/(tabs)/create.jsx
@@ -134,8 +134,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 28,
     fontWeight: "bold",
-    marginTop: 30,
-    marginBottom: 30,
+    marginBottom: 20,
   },
 
   input: {
